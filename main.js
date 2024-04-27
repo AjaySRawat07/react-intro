12ee26db764fa0b23bec337061d263a4728f81bd
Ajay Singh Rawat <149299603+AjaySRawat07@users.noreply.github.com>
2024-04-27 21:42:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const H1 = React.createElement("h1", {}, "Namaste React");
-const Para = React.createElement("p", { id: "para" }, "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, amet.");
+const div1 = React.createElement("div", { id: "div" },
+                                 React.createElement("p", { id: "para" }, "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, amet."););
 const Btn = React.createElement("button", { id: "b1" }, "Click here");
-const Div = React.createElement("div", { id: "main" }, [H1, Para, Btn]);
+const Div = React.createElement("div", { id: "main" }, [H1, div1, Btn]);
 
 ReactDOM.render(Div, document.querySelector(".root"));
