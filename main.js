32304712bf299af49972dfa57d23ccb541dba8a9
Ajay Singh Rawat <149299603+AjaySRawat07@users.noreply.github.com>
2024-04-30 23:55:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
-const H1 = React.createElement("h1", {}, "Namaste React");
-const div1 = React.createElement("div", { id: "div" },
-                                 React.createElement("p", { id: "para" }, "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde, amet."););
-const Btn = React.createElement("button", { id: "b1" }, "Click here");
-const Div = React.createElement("div", { id: "main" }, [H1, div1, Btn]);
+const H1 = React.createElement('h1',{},'i am H1')
+const P = React.createElement('p',{},'i am paragrapy inside 1st Div')
+const Div = React.createElement('div',{},P)
 
-ReactDOM.render(Div, document.querySelector(".root"));
+const P1 = React.createElement('p',{},'i am second p tag inside 2nd div')
+const A = React.createElement('a',{href:'Home'},'i am ancher tag')
+const Div2 = React.createElement('div',{},[P,A])
+
+const P2 = React.createElement('p',{},'i am second paragrapy inside 3st Div')
+const Div3 = React.createElement('div',{},P2)
+const Div4 = React.createElement('div',{},H1,Div,Div2,Div3)
+
+ReactDOM.render(Div4,document.getElementById('root'))
